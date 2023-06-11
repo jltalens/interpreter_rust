54efee00c94ce23954286cb472eae58fa0b7050b
Jose <jose@minehub.com>
2023-06-11 12:17:44 +0100

    It can now parse the let statements

--- src/ast/ast.rs
+++ src/ast/ast.rs
@@ -1,70 +1,22 @@
 use crate::token::token::Token;
 
+pub type Program = Statements;
 
-pub trait Node{
-    fn token_literal(&self) -> String;
-}
+pub type Statements = Vec<Statement>;
 
-pub trait Statement: Node {
-    fn statement_type(&self) -> &str;
-    fn statement_node(&self);
-}
-
-impl dyn Statement {
-    pub unsafe fn downcast_ref_unchecked<T: Statement>(&self) -> &T {
-        &*(self as *const dyn Statement as *const T)
-    }
-}
-
-pub trait Expression: Node{
-    fn expression_node(&self);
-}
-
-pub struct Program {
-    pub statements: Vec<Box<dyn Statement>>,
-}
-
-impl Node for Program {
-    fn token_literal(&self) -> String {
-        if self.statements.len() > 0 {
-            self.statements[0].token_literal()
-        } else {
-            String::from("")
-        }
-    }
-}
-
+#[derive(Debug, Clone)]
 pub struct Identifier {
     pub token: Token,
     pub value: String,
 }
 
-impl Expression for Identifier {
-    fn expression_node(&self) {}
-}
-
-impl Node for Identifier {
-    fn token_literal(&self) -> String {
-        self.token.literal.clone()
-    }
-}
-
-
-pub struct LetStatement {
+#[derive(Debug, Clone)]
+pub struct Expression {
     pub token: Token,
-    pub name: Identifier,
-    pub value: Box<dyn Expression>,
+    pub value: String,
 }
 
-impl Statement for LetStatement{
-    fn statement_type(&self) -> &str{
-        "LetStatement"
-    }
-    fn statement_node(&self) {}
-}
-
-impl Node for LetStatement {
-    fn token_literal(&self) -> String {
-        self.token.literal.clone()
-    }
+#[derive(Debug)]
+pub enum Statement {
+    LetStatement(Identifier, Expression),
 }
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,7 +1,7 @@
 use crate::{
-    ast::ast::Program,
+    ast::ast::{Expression, Identifier, Program, Statement},
     lexer::lexer::{Lexer, LexerIterItem},
-    token::token::Token,
+    token::token::{Token, Tokens},
 };
 use std::cell::Cell;
 
@@ -28,13 +28,71 @@ impl Parser {
         self.peek_token = self.lexer.get_mut().next();
     }
 
-    pub fn parse_program(&self) -> Option<Program> {
-        todo!()
+    pub fn parse_program(&mut self) -> Program {
+        let program = Program::new();
+        self.int_parse_program(program)
+    }
+
+    fn int_parse_program(&mut self, mut program: Program) -> Program {
+        match self.current_token.clone() {
+            Some(token) if token.token_type != Tokens::EOF => match self.parse_statement() {
+                Some(stmt) => {
+                    program.push(stmt);
+                    self.next_token();
+                    self.int_parse_program(program)
+                }
+                _ => {
+                    self.next_token();
+                    self.int_parse_program(program)
+                }
+            },
+            _ => program,
+        }
+    }
+
+    fn parse_statement(&mut self) -> Option<Statement> {
+        match self.current_token.as_ref() {
+            Some(token) if token.token_type == Tokens::LET => self.parse_let(),
+            _ => None,
+        }
+    }
+
+    fn parse_let(&mut self) -> Option<Statement> {
+        if !self.expected_token(Tokens::IDENT) {
+            return None;
+        }
+
+        let identifier = Identifier {
+            token: self.current_token.clone().unwrap(),
+            value: self.current_token.clone().unwrap().literal,
+        };
+        if !self.expected_token(Tokens::ASSIGN) {
+            return None;
+        }
+        while !(self.current_token.clone().unwrap().token_type != Tokens::SEMICOLON) {
+            self.next_token();
+        }
+        let expression = Expression {
+            token: self.current_token.clone().unwrap(),
+            value: self.current_token.clone().unwrap().literal,
+        };
+        Some(Statement::LetStatement(identifier, expression))
+    }
+
+    fn expected_token(&mut self, token_type: Tokens) -> bool {
+        match self.peek_token.clone() {
+            Some(token) if token.token_type == token_type => {
+                self.next_token();
+                true
+            }
+            _ => false,
+        }
     }
 }
 #[cfg(test)]
 mod parser_tester {
-    use crate::{lexer::lexer::Lexer, ast::ast::{LetStatement, Statement}};
+    use crate::ast::ast::Statement::LetStatement;
+    use crate::lexer::lexer::Lexer;
 
     use super::Parser;
 
@@ -47,20 +105,20 @@ mod parser_tester {
         ";
 
         let lexer = Lexer::new(String::from(input));
-        let parser = Parser::new(lexer);
+        let mut parser = Parser::new(lexer);
 
-        let statements = parser.parse_program().unwrap().statements;
+        let program = parser.parse_program();
 
-        assert_eq!(statements.len(), 3);
+        assert_eq!(program.len(), 3);
 
-        let _expected_identifiers = vec!["x", "y", "foobar"];
+        let mut expected_identifiers = vec!["x", "y", "foobar"];
 
-        for statement in statements.into_iter() {
-            assert_eq!(statement.token_literal(), "let");
-            assert_eq!(statement.statement_type(), "LetStatement");
-            let let_statement = unsafe { statement.downcast_ref_unchecked::<LetStatement>() };
-            assert_eq!(let_statement.name.value, "x");
+        for statement in program.into_iter() {
+            match statement {
+                LetStatement(identifier, _) => {
+                    assert_eq!(identifier.value, expected_identifiers.remove(0));
+                }
+            }
         }
-
     }
 }
